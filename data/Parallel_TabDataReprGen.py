bdf2a915fa2ae4dcfa92227b8b6e9c3e946d56f1
andywiggins <andy.wiggins.94@gmail.com>
2019-06-19 11:16:47 -0400

    Update Parallel_TabDataReprGen.py

--- data/Parallel_TabDataReprGen.py
+++ data/Parallel_TabDataReprGen.py
@@ -11,6 +11,6 @@ mode_list = [modes[0]] * 360 + [modes[1]] * 360 + [modes[2]] * 360 + [modes[3]] 
 
 
 if __name__ == "__main__":
-    # number of processes will run simeltaneously
+    # number of processes will run simultaneously
     pool = Pool(11)
     results = pool.map(main, zip(filename_indices, mode_list))
